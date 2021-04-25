5950f4067762c7480b1b745d8567f40877f1d5ad
Strykeros <Strykeros-PC>
2021-04-25 23:18:48 +0300

    Add project files.

---
+++ Game.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace AnotherPacman
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelGameOver = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelGameOver
+            // 
+            this.labelGameOver.AutoSize = true;
+            this.labelGameOver.Font = new System.Drawing.Font("Ravie", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGameOver.ForeColor = System.Drawing.Color.Yellow;
+            this.labelGameOver.Location = new System.Drawing.Point(45, 138);
+            this.labelGameOver.Name = "labelGameOver";
+            this.labelGameOver.Size = new System.Drawing.Size(454, 81);
+            this.labelGameOver.TabIndex = 0;
+            this.labelGameOver.Text = "Game Over";
+            this.labelGameOver.Visible = false;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Matura MT Script Capitals", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ScoreLabel.Location = new System.Drawing.Point(32, 447);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(88, 40);
+            this.ScoreLabel.TabIndex = 1;
+            this.ScoreLabel.Text = "label1";
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 496);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.labelGameOver);
+            this.DoubleBuffered = true;
+            this.Name = "Game";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelGameOver;
+        private System.Windows.Forms.Label ScoreLabel;
+    }
+}
+
